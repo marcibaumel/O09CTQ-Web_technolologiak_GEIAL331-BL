500de5efabc21ac95990b25b15e99b880a83e1bc
Baumel Márton <marcibaumel@gmail.com>
2021-09-20 10:09:37 +0200

    Working servlet

---
+++ Webalaklmazasok Java/02/02gyak/src/main/java/Servlet2.java
@@ -0,0 +1,7 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "servlet2", urlPatterns = "/servlet2")
+public class Servlet2 extends HttpServlet {
+
+}
