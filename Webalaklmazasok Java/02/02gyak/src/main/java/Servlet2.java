b739673752bb5656f6f5096ddce4063e29d7cb9e
Baumel Márton <marcibaumel@gmail.com>
2021-09-20 10:50:15 +0200

    Servlet2

--- Webalaklmazasok Java/02/02gyak/src/main/java/Servlet2.java
+++ Webalaklmazasok Java/02/02gyak/src/main/java/Servlet2.java
@@ -1,7 +1,18 @@
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "servlet2", urlPatterns = "/servlet2")
 public class Servlet2 extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Yeaaah I can do loging stuff");
+        resp.getWriter().println("Hi from servlet 2");
+    }
 }
