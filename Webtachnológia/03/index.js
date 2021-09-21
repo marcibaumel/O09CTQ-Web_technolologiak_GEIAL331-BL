ddaf836ab87571cec512c59fb69d970077ee2ea6
Baumel Márton <marcibaumel@gmail.com>
2021-09-21 17:19:48 +0200

    f

---
+++ Webtachnológia/03/index.js
@@ -0,0 +1,9 @@
+$("#submit-btn").click(function(){
+    //alert("Hello");
+    var fname = $("#fname").val();
+    var lname = $("#lname").val();
+    //alert(fname);
+    $("#firstName").text(fname);
+    $("#lastName").text(lname);
+
+})
