740e7fc92d424e5f7d453cc3e2cd6da918d7cc01
Baumel Márton <marcibaumel@gmail.com>
2021-09-29 10:50:53 +0200

    WIP: Omdb data load

--- assignment/omdb.js
+++ assignment/omdb.js
@@ -19,17 +19,35 @@ function getOmdbData(){
 
 var realData;
 
-$.getJSON('http://www.omdbapi.com/?apikey=f51c1d39&t=Kate', function(data) {
+function getData(){
+    var name =$("#inputFilmTitle").val();
+    var year = $("#inputFilmYear").val();
+    console.log(name);
+    $.getJSON("http://www.omdbapi.com/?apikey=f51c1d39&t="+name+"&year="+year, function(data) {
     console.log(data.Title);
     realData = data; 
 });
+}
 
 
 $("#omdb-submit-btn").click(function(){
-
-    if(realData.Title == undefined){
-        alert("Somethings went wrong!");
+    getData();
+    
+    if(typeof realData.Title !== 'undefined'){
+        alert("Somethings went wrong! Check your answers. ");
     }
-    //console.log(realData.Title);
-    $("#poster").attr("src",realData.Poster);
+   
+    setTableBorder();
+    
+    $("#film-poster").attr("src",realData.Poster);
+    $("#film-title").text(realData.Title);
+    $("#film-release").text(realData.Year);
+    $("#film-runtime").text(realData.Runtime);
 });
+
+function setTableBorder(){
+    console.log($("#film-title").text());
+    if($("#film-title").text() != ""){
+        $("#omdb-table").addClass("omdb-border");
+    }
+}
