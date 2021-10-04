7de59a3dbc34a1fcb10514edf33174ddabae0535
Baumel Márton <marcibaumel@gmail.com>
2021-10-04 12:02:40 +0200

    WIP: animation

---
+++ assignment/index.js
@@ -0,0 +1,18 @@
+/*
+const imgUrl = "/imgs/phone2.png";
+//$("#phone-img").fadeIn();
+
+
+const images = document.getElementsByTagName("img");
+
+for (let image of images) {
+  image.addEventListener("load", fadeImg);
+  image.style.opacity = "0";
+}
+
+function fadeImg () {
+  this.style.transition = "opacity 2s";
+  this.style.opacity = "1";
+}
+
+*/
