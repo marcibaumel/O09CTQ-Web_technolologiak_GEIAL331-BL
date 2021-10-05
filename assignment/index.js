0676342b2c77baa5111772e5d220eddfa821df1e
Baumel Márton <marcibaumel@gmail.com>
2021-10-05 16:52:18 +0200

    gyak05_init

--- assignment/index.js
+++ assignment/index.js
@@ -1,14 +1,3 @@
-/*
-const imgUrl = "/imgs/phone2.png";
-//$("#phone-img").fadeIn();
-
-
-
-$("#phone-img").load(function(){
-  $("#phone-img").fadeIn();
-})
-
-*/
 $(".img-p").mouseover(function(){
   $(this).animate({
     height: '+=20px',
